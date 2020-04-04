93fac65ca1e7a0760caad475b710e7048a0da186
soura49 <soura_balakrishna49@yahoo.com>
2020-04-04 19:55:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ import setuptools
 
 
 setuptools.setup(
-    name="currentclidate",
+    name="currentcli",
     version="1.0.0",
     author="balakrishna Soura",
     email="soura_balakrishna49@yahoo.com",
     description="A small example package",
     long_description="long_description",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.6',
+    entry_points = {
+        'console_scripts': [
+            'currentcli = commands.__main__:main'
+        ]
+    }
 )
